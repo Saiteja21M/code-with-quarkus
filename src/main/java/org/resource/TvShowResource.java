0c95de151f983fe7c63f00861b37a7a22961aca3
saiteja <saiteja.koppala@nttadata.com>
2025-05-22 19:24:32 +0530

    add logger

--- src/main/java/org/resource/TvShowResource.java
+++ src/main/java/org/resource/TvShowResource.java
@@ -7,7 +7,9 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 import org.entity.TvShow;
+import org.jboss.logging.Logger;
 import org.service.TvShowService;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -17,11 +19,16 @@ public class TvShowResource {
     @Inject
     TvShowService tvShowService;
 
+    @Inject
+    Logger logger;
+
     @GET
     @Produces("application/json")
     public Response getTvShow(@QueryParam("showName") String showName) {
+        logger.info("searched for " + showName);
         List<TvShow> tvShows = tvShowService.getTvShow(showName);
         if (tvShows != null && !tvShows.isEmpty()) {
+            logger.info("received result " + tvShows);
             return Response.ok(tvShows).build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
